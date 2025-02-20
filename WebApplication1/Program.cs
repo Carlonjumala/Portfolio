5117a1c2a5d65ffbcd6f5ea4f68fcd101cfcb514
Carlonjumala <leevi.nokkonen@gmail.com>
2025-02-20 19:56:59 +0200

    net8

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Portfolio.Data;  // Correct namespace here
+using Portfolio.Data; 
 using Portfolio.Models;
 using Portfolio.Services;
 
