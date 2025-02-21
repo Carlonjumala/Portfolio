decb0cab7efa7466a05378461949703aed3ad706
Carlonjumala <leevi.nokkonen@gmail.com>
2025-02-21 13:25:47 +0200

    asd

--- WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ab71b95725522a8464d9778773b77987b74351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8d9bbb79b220d2adb58729c2afea23abf11f64")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
